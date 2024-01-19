9f5af5a35bacf268669c8aac2d43ec487b92a841
taryncodes@github.com <taryn.bilsborough@gmail.com>
2024-01-19 15:51:56 +0000

    Improvements on the UI with statements to let the user know if they have already guessed the letter and if the letter is not in the word

--- hangman_project.py
+++ hangman_project.py
@@ -3,6 +3,7 @@ import random
 import hangman_art
 import hangman_words
 
+print(hangman_art.logo)
 end_of_game = False
 chosen_word = random.choice(hangman_words.word_list)
 
@@ -15,12 +16,16 @@ for _ in chosen_word:
 while "_" in display:
     guess = input("Guess a letter: ").lower()
 
+    if guess in display:
+        print(f"You've already used the letter {guess}")
+
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
 
     if guess not in chosen_word:
+        print(f"The letter {guess} is not in the word, you lose a life :(")
         lives -= 1
         if lives == 0:
             end_of_game = True
